3ac2563df4c300ca1b1f619477f8b3979e7d452c
DaveTheBarbarian <davidstnl83@gmail.com>
2017-09-30 00:46:14 -0400

    Done

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import java.util.Random;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
